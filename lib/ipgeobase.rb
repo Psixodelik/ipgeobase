9cf973f60e53d422f883befe478f8adba8551dd0
Nikita Mikhaylov <nmikhaylov92@gmail.com>
2021-08-28 14:41:25 +0300

    add DataParser class

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "addressable/uri"
+require 'net/http'
 
 module Ipgeobase
   class Error < StandardError; end
   # Your code goes here...
+  autoload 'DataParse', 'ipgeobase/dataparser'
+
+  def self.lookup(adress)
+    api_uri = Addressable::URI.parse("http://ip-api.com/xml/#{adress}")
+    dataApi = api_answer(api_uri)
+
+    DataParse.parse(dataApi)
+  end
+
+  def self.api_answer(uri)
+    Net::HTTP.get(uri)
+  end
 end
---
+++ lib/ipgeobase/dataparser.rb
@@ -0,0 +1,14 @@
+require 'happymapper'
+
+module Ipgeobase
+  class DataParse
+    include HappyMapper
+    
+    tag 'query'
+    element :city, String
+    element :country, String
+    element :countryCode, String
+    element :lat, Float
+    element :lon, Float
+  end
+end
